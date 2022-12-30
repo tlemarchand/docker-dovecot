530617e1da428e4e20d5d83a97cad7d6155ae598
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2022-12-30 10:52:43 +0100

    2.3.20-3

--- release.sh
+++ release.sh
@@ -6,11 +6,11 @@ podman run --name dovecot debian:bullseye-slim /bin/bash -c "apt-get update > /d
   curl https://repo.dovecot.org/DOVECOT-REPO-GPG | gpg --import && \
   gpg --export ED409DA1 > /etc/apt/trusted.gpg.d/dovecot.gpg && \
   echo 'deb https://repo.dovecot.org/ce-2.3-latest/debian/bullseye bullseye main' > /etc/apt/sources.list.d/dovecot.list && \
-  apt-get update > /dev/null && apt-cache policy dovecot-core | sed -n -e 's/^.*Candidate: //p' | tr -d '\n' | cut -d~ -f 1 | cut -d+ -f 1 | cut -d: -f 2" > version
+  apt-get update > /dev/null && apt-cache policy dovecot-core | sed -n -e 's/^.*Candidate: //p' | tr -d '\n'" > version
 podman rm dovecot
 
 git add -A
-git commit -m `cat version`
+git commit -m `cat version | cut -d~ -f 1 | cut -d+ -f 1 | cut -d: -f 2`
 git push origin main
-git tag -f `cat version`
-git push origin -f `cat version`
+git tag -f `cat version | cut -d~ -f 1 | cut -d+ -f 1 | cut -d: -f 2`
+git push origin -f `cat version | cut -d~ -f 1 | cut -d+ -f 1 | cut -d: -f 2`
