2b6fd981eff72c90bf3e9f795203065504f45a61
Thomas Lemarchand <thomas-gh@lemarchand.io>
2025-06-07 20:31:41 +0200

    2.3.21.1-2

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 git checkout main
 
 podman run --name dovecot debian:bullseye-slim /bin/bash -c "apt-get update > /dev/null && apt-get install -y curl apt-transport-https gpg > /dev/null && \
-  curl https://repo.dovecot.org/DOVECOT-REPO-GPG | gpg --import && \
+  curl https://repo.dovecot.org/DOVECOT-REPO-GPG-2.3 | gpg --import && \
   gpg --export ED409DA1 > /etc/apt/trusted.gpg.d/dovecot.gpg && \
   echo 'deb https://repo.dovecot.org/ce-2.3-latest/debian/bullseye bullseye main' > /etc/apt/sources.list.d/dovecot.list && \
   apt-get update > /dev/null && apt-cache policy dovecot-core | sed -n -e 's/^.*Candidate: //p' | tr -d '\n'" > version
